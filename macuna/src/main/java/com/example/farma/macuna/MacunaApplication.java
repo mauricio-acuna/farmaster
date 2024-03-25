fa2dafeeb4cbb0b49cf86f1d75ac59a762c5399a
Mauricio Acuna <mauricioacu@gmail.com>
2024-03-25 18:56:09 +0100

    Add files via upload

---
+++ macuna/src/main/java/com/example/farma/macuna/MacunaApplication.java
@@ -0,0 +1,13 @@
+package com.example.farma.macuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacunaApplication.class, args);
+	}
+
+}
---
+++ macuna/src/test/java/com/example/farma/macuna/MacunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.farma.macuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
